7d71fbc2bd6052a22f6a0565049c2e91ae97beb6
Sasidhar Veerni <sasidharveerni2@gmail.com>
2024-03-09 14:17:47 +0530

    New Changes

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ function showValues(event) {
     // console.log(this.innerText)
     const value = this.innerText;
     const display = document.getElementsByClassName("child-1")[0]; // Access the display element
+    
+    var elements = document.getElementsByClassName("child-1");
+    if(display.innerText.length > 32){
+        for (var i = 0; i < elements.length; i++) {
+                elements[i].style.fontSize = "1.75em";
+            }
+
+    }
 
     // Check if the clicked button is the RESET button
     if (this.id === 'reset') {
@@ -17,9 +25,12 @@ function showValues(event) {
       else if (this.id === 'equal') {
         // console.log(display.innerText);
         try {
+            // console.log(display.innerText.length);
+            // console.log(display.innerText);
             const calculation = display.innerText.replace(/[^-()\d/*+.]/g, ''); // Basic cleanup to ensure only valid characters are processed
             const res = eval(calculation);
             display.innerHTML = `<span style='color: #fff;'>${res}</span>`;
+            
         } catch (error) {
             display.innerHTML = `<span style='color: #fff;'>Error</span>`; // Display error message or handle errors as needed
         }
